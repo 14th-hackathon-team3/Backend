ff4857cac4daf8c597b1b4b9b8a3b9ba52b3785a
bomee1123 <bomee1123@ewha.ac.kr>
2026-08-13 02:54:47 +0900

    feat: 초대코드 검증 API 구현

--- groups/urls.py
+++ groups/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GuardianOnboardingView
+from .views import GuardianOnboardingView, InviteCodeCheckView
  
 urlpatterns = [
     path("membership/onboarding/", GuardianOnboardingView.as_view(), name="guardian-onboarding"),
+    path("invite/<str:invite_code>/", InviteCodeCheckView.as_view(), name="invite-check"),
 ]
